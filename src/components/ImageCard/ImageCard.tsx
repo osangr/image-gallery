8d2c1da3d6191479dd29091f50d026e2bb6326fe
oscarsangpa <oscarsangrador@gmail.com>
2026-03-12 14:12:48 +0100

    refactor: extract ImageCard component

---
+++ src/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,44 @@
+import type { Photo } from "../../types";
+import styles from "./ImageCard.module.scss";
+
+interface ImageCardProps {
+  photo: Photo;
+  onRemove: (uniqueId: string) => void;
+  onTransitionEnd: (uniqueId: string) => void;
+  isRemoving: boolean;
+}
+
+export function ImageCard({
+  photo,
+  onRemove,
+  onTransitionEnd,
+  isRemoving,
+}: ImageCardProps) {
+  const { author, uniqueId, id } = photo;
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter" || e.key === " ") {
+      onRemove(uniqueId);
+    }
+  };
+
+  return (
+    <article
+      onClick={() => onRemove(uniqueId)}
+      onTransitionEnd={() => onTransitionEnd(uniqueId)}
+      className={`${styles.photoCard} ${isRemoving ? styles.removing : ""}`}
+      tabIndex={0}
+      role="button"
+      aria-label={`Eliminar foto de ${author}`}
+      onKeyDown={handleKeyDown}
+    >
+      <figure>
+        <img
+          src={`https://picsum.photos/id/${id}/200/200`}
+          alt={`Imagen de ${author}`}
+        />
+        <figcaption>{author}</figcaption>
+      </figure>
+    </article>
+  );
+}
