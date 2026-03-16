be39bfd0aee5be4bdc37b12c67e48d5c2accddf2
oscarsangpa <oscarsangrador@gmail.com>
2026-03-16 09:38:15 +0100

    fix: improve accessibility in Spinner, EmptyState and html lang

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -2,7 +2,7 @@ import styles from "./EmptyState.module.scss";
 
 export function EmptyState({ message }: { message: string }) {
   return (
-    <div className={styles.emptyState}>
+    <div className={styles.emptyState} role="status">
       <p>{message}</p>
     </div>
   );
